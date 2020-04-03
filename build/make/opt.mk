632f329d006d53e2b0de447fd0b46e09f670a7b5
André van Schoubroeck <andre@blaatschaap.be>
2020-04-03 17:23:47 +0200

    Set DEBUG and RELEASE defines

--- build/make/opt.mk
+++ build/make/opt.mk
@@ -21,14 +21,17 @@ endif
 ifeq ($(COMPILER_TYPE), GCC)
 	ifeq ($(BUILD_MODE), DEBUG)
 		OPT ?= -O0 -g3
+		CFLAGS += -DDEBUG
 	endif
 
 	ifeq ($(BUILD_MODE), RELEASE_SIZE)
 		OPT ?= -Os
+		CFLAGS += -DRELEASE
 	endif
 
 	ifeq ($(BUILD_MODE), RELEASE_SPEED)
 		OPT ?= -O2
+		CFLAGS += -DRELEASE
 	endif
 endif 
 
@@ -36,15 +39,19 @@ endif
 
 ifeq ($(COMPILER_TYPE), SDCC)
 	ifeq ($(BUILD_MODE), DEBUG)
+		# TODO What flags are required for an SDCC debug build?
 		OPT ?= 
+		CFLAGS += -DDEBUG
 	endif
 
 	ifeq ($(BUILD_MODE), RELEASE_SIZE)
 		OPT ?= --opt-code-size
+		CFLAGS += -DRELEASE
 	endif
 
 	ifeq ($(BUILD_MODE), RELEASE_SPEED)
 		OPT ?= --opt-code-speed
+		CFLAGS += -DRELEASE
 	endif
 endif 
 
