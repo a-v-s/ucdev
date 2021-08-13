e5f4b8a13206f2536527805479182f60ddace48e
André van Schoubroeck <andre@blaatschaap.be>
2021-08-13 12:03:06 +0200

    Testing with different SPI flash, no luck

--- demos/spi/main.c
+++ demos/spi/main.c
@@ -61,27 +61,27 @@ void SystemClock_Config(void) {
 
 uint8_t test_adxl_spi_read_id() {
 
-		static bshal_spim_t adxl_spi_config;
-		adxl_spi_config.frequency = 1000000;
-		adxl_spi_config.bit_order = 0; //MSB
-		adxl_spi_config.mode = 0;
+	static bshal_spim_t adxl_spi_config;
+	adxl_spi_config.frequency = 1000000;
+	adxl_spi_config.bit_order = 0; //MSB
+	adxl_spi_config.mode = 0;
 
-		adxl_spi_config.hw_nr = 2; // SPI2
-		adxl_spi_config.miso_pin = bshal_gpio_encode_pin(GPIOB, GPIO_PIN_14);
-		adxl_spi_config.mosi_pin = bshal_gpio_encode_pin(GPIOB, GPIO_PIN_15);
-		adxl_spi_config.sck_pin = bshal_gpio_encode_pin(GPIOB, GPIO_PIN_13);
+	adxl_spi_config.hw_nr = 2; // SPI2
+	adxl_spi_config.miso_pin = bshal_gpio_encode_pin(GPIOB, GPIO_PIN_14);
+	adxl_spi_config.mosi_pin = bshal_gpio_encode_pin(GPIOB, GPIO_PIN_15);
+	adxl_spi_config.sck_pin = bshal_gpio_encode_pin(GPIOB, GPIO_PIN_13);
 
-		adxl_spi_config.nss_pin = bshal_gpio_encode_pin(GPIOA,GPIO_PIN_10);
-		adxl_spi_config.nrs_pin = -1;
-		adxl_spi_config.ncd_pin = -1;
-		adxl_spi_config.irq_pin = -1;
+	adxl_spi_config.nss_pin = bshal_gpio_encode_pin(GPIOA, GPIO_PIN_10);
+	adxl_spi_config.nrs_pin = -1;
+	adxl_spi_config.ncd_pin = -1;
+	adxl_spi_config.irq_pin = -1;
 
-		bshal_spim_init(&adxl_spi_config);
+	bshal_spim_init(&adxl_spi_config);
 
 	uint8_t result = -1;
 
 	// device id is register 0 // Read us 0x80
-	uint8_t get_id = 0x00 |  0x80;
+	uint8_t get_id = 0x00 | 0x80;
 	bshal_spim_transmit(&adxl_spi_config, &get_id, 1, true);
 
 	// expected o345 // 0xe5
@@ -90,8 +90,6 @@ uint8_t test_adxl_spi_read_id() {
 	//return result;
 	return get_id;
 }
-
-
 
 uint32_t test_sdmmc_spi_read_id() {
 	static bshal_spim_t flash_sdmmc_config;
@@ -104,7 +102,7 @@ uint32_t test_sdmmc_spi_read_id() {
 	flash_sdmmc_config.mosi_pin = bshal_gpio_encode_pin(GPIOB, GPIO_PIN_15);
 	flash_sdmmc_config.sck_pin = bshal_gpio_encode_pin(GPIOB, GPIO_PIN_13);
 
-	flash_sdmmc_config.nss_pin = bshal_gpio_encode_pin(GPIOA,GPIO_PIN_8);
+	flash_sdmmc_config.nss_pin = bshal_gpio_encode_pin(GPIOA, GPIO_PIN_8);
 	flash_sdmmc_config.nrs_pin = -1;
 	flash_sdmmc_config.ncd_pin = -1;
 	flash_sdmmc_config.irq_pin = -1;
@@ -122,31 +120,29 @@ uint32_t test_sdmmc_spi_read_id() {
 	bshal_spim_receive(&flash_sdmmc_config, &init, sizeof(init), false);
 	// This returns us all zeros. We are expecting some bits to be set.
 
+	uint8_t result = -1;
 
-
-uint8_t result = -1;
-
-uint32_t device_id = 0;
+	uint32_t device_id = 0;
 }
 
 uint32_t test_flash_spi_read_id() {
 
-		static bshal_spim_t flash_spi_config;
-		flash_spi_config.frequency = 1000000;
-		flash_spi_config.bit_order = 0; //MSB
-		flash_spi_config.mode = 0;
+	static bshal_spim_t flash_spi_config;
+	flash_spi_config.frequency = 1000000;
+	flash_spi_config.bit_order = 0; //MSB
+	flash_spi_config.mode = 0;
 
-		flash_spi_config.hw_nr = 2; // SPI2
-		flash_spi_config.miso_pin = bshal_gpio_encode_pin(GPIOB, GPIO_PIN_14);
-		flash_spi_config.mosi_pin = bshal_gpio_encode_pin(GPIOB, GPIO_PIN_15);
-		flash_spi_config.sck_pin = bshal_gpio_encode_pin(GPIOB, GPIO_PIN_13);
+	flash_spi_config.hw_nr = 2; // SPI2
+	flash_spi_config.miso_pin = bshal_gpio_encode_pin(GPIOB, GPIO_PIN_14);
+	flash_spi_config.mosi_pin = bshal_gpio_encode_pin(GPIOB, GPIO_PIN_15);
+	flash_spi_config.sck_pin = bshal_gpio_encode_pin(GPIOB, GPIO_PIN_13);
 
-		flash_spi_config.nss_pin = bshal_gpio_encode_pin(GPIOB,GPIO_PIN_12);
-		flash_spi_config.nrs_pin = -1;
-		flash_spi_config.ncd_pin = -1;
-		flash_spi_config.irq_pin = -1;
+	flash_spi_config.nss_pin = bshal_gpio_encode_pin(GPIOB, GPIO_PIN_12);
+	flash_spi_config.nrs_pin = -1;
+	flash_spi_config.ncd_pin = -1;
+	flash_spi_config.irq_pin = -1;
 
-		bshal_spim_init(&flash_spi_config);
+	bshal_spim_init(&flash_spi_config);
 
 	uint8_t result = -1;
 
@@ -158,33 +154,41 @@ uint32_t test_flash_spi_read_id() {
 	// So, this means, the data comes from the chip... but something else is off...
 	// The same results by another flash chip so it wasn't a bad chip.
 
-
-
 	{
-	uint8_t get_id = 0x9F;
-	bshal_spim_transmit(&flash_spi_config, &get_id, 1, true);
-	bshal_spim_receive(&flash_spi_config, &device_id, 2, false);
-	//0xEE  expect 4016
+		uint8_t get_id = 0x9F;
+		bshal_spim_transmit(&flash_spi_config, &get_id, 1, true);
+		bshal_spim_receive(&flash_spi_config, &device_id, 2, false);
+		//W25Q32JV: 0xEE  expect 4016
+		//GD25Q32C: 0xC0  expect 4016
 	}
 	{
-	uint8_t get_id[4] = {0xAB, 0x00, 0x00, 0x00};
-	bshal_spim_transmit(&flash_spi_config, get_id,4, true);
-	bshal_spim_receive(&flash_spi_config, &device_id, 1, false);
-	// got 0x00  expect 15
+		uint8_t get_id[4] = { 0xAB, 0x00, 0x00, 0x00 };
+		bshal_spim_transmit(&flash_spi_config, get_id, 4, true);
+		bshal_spim_receive(&flash_spi_config, &device_id, 1, false);
+		//W25Q32JV: got 0x00  expect 15
+		//GD25Q32C: got 0x15  expect 0x15 : OK
 	}
 	{
-	uint8_t get_id[4] = {0x90, 0x00, 0x00, 0x00};
-	bshal_spim_transmit(&flash_spi_config, get_id,4, true);
-	bshal_spim_receive(&flash_spi_config, &device_id, 2, false);
-	// got 0x0eee // expect ef
+		uint8_t get_id[4] = { 0x90, 0x00, 0x00, 0x00 };
+		bshal_spim_transmit(&flash_spi_config, get_id, 4, true);
+		bshal_spim_receive(&flash_spi_config, &device_id, 2, false);
+		//W25Q32JV: got 0x0eee // expect ef
+		//GD25Q32C: got 0xcc0 // expect 15 c8
 	}
-
 
 	// Expected values for W25Q32JV :
-	// MF7 - MF0 = EFh			// answer to 90
+
 	// ID7 - ID0 = 15h			// answer to ABh, 90h, 92h, 94h
-	// ID15 - ID0 = 4016h		// answer to 9F
-	//return result;
+	// answer to 90		//							MF7 - MF0 = EFh
+	// answer to 9F 	//	ID15 - ID0 = 4016h
+
+
+	// Values for GD25Q32C (page 15)
+	// ANSWER to 90		//					ID7-ID0:	15	MID = 0xc8
+	// ANSWER to 9F		//	ID15-ID8: 40	ID7-ID0: 	16
+	// ANSWER TO AB		//					ID7-ID0:	15
+
+
 	return device_id;
 }
 
@@ -199,12 +203,11 @@ void screen_init() {
 	screen_spi_config.mosi_pin = bshal_gpio_encode_pin(GPIOB, GPIO_PIN_15);
 	screen_spi_config.sck_pin = bshal_gpio_encode_pin(GPIOB, GPIO_PIN_13);
 
-	screen_spi_config.ncd_pin = bshal_gpio_encode_pin(GPIOB,GPIO_PIN_9);
-	screen_spi_config.nrs_pin = bshal_gpio_encode_pin(GPIOB,GPIO_PIN_8);
-	screen_spi_config.nss_pin = bshal_gpio_encode_pin(GPIOB,GPIO_PIN_7);
+	screen_spi_config.ncd_pin = bshal_gpio_encode_pin(GPIOB, GPIO_PIN_9);
+	screen_spi_config.nrs_pin = bshal_gpio_encode_pin(GPIOB, GPIO_PIN_8);
+	screen_spi_config.nss_pin = bshal_gpio_encode_pin(GPIOB, GPIO_PIN_7);
 
-
-	screen_spi_config.irq_pin = -1;//
+	screen_spi_config.irq_pin = -1; //
 
 	bshal_spim_init(&screen_spi_config);
 
@@ -217,7 +220,7 @@ void screen_init() {
 	// Thinking about we would need some SPI manager
 }
 
-void rfid5_init(rc52x_t *rc52x){
+void rfid5_init(rc52x_t *rc52x) {
 	static bshal_spim_t rfid_spi_config;
 	rfid_spi_config.frequency = 10000000; // SPI speed for MFRC522 = 10 MHz
 	rfid_spi_config.bit_order = 0; //MSB
@@ -228,7 +231,6 @@ void rfid5_init(rc52x_t *rc52x){
 	rfid_spi_config.mosi_pin = bshal_gpio_encode_pin(GPIOB, GPIO_PIN_15);
 	rfid_spi_config.sck_pin = bshal_gpio_encode_pin(GPIOB, GPIO_PIN_13);
 
-
 	rfid_spi_config.nss_pin = bshal_gpio_encode_pin(GPIOB, GPIO_PIN_6);
 	rfid_spi_config.nrs_pin = bshal_gpio_encode_pin(GPIOB, GPIO_PIN_5);
 	rfid_spi_config.irq_pin = bshal_gpio_encode_pin(GPIOA, GPIO_PIN_3);
@@ -237,7 +239,7 @@ void rfid5_init(rc52x_t *rc52x){
 
 	bshal_spim_init(&rfid_spi_config);
 	rc52x->transport = mfrc_transport_spi;
-	rc52x->transport_config=&rfid_spi_config;
+	rc52x->transport_config = &rfid_spi_config;
 	rc52x->delay_ms = bshal_delay_ms;
 }
 
@@ -254,10 +256,9 @@ int main() {
 	bshal_delay_us(10);
 	screen_init();
 
-	print("Hello World!",3);
+	print("Hello World!", 3);
 
 	rfid5_init(&g_rc52x);
-
 
 	char str[32];
 	uint8_t version = -1;
@@ -265,11 +266,9 @@ int main() {
 	sprintf(str, "MFRC522 %02X", version);
 	print(str, 4);
 
-
 	version = test_adxl_spi_read_id();
 	sprintf(str, "ADXL ID  %03o", version);
 	print(str, 5);
-
 
 	uint32_t flash_id = test_flash_spi_read_id();
 
