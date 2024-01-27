24ceb336acf49b7c426655d3481759a591a2ca1e
André van Schoubroeck <andre@blaatschaap.be>
2024-01-27 15:31:27 +0100

    protocol

---
+++ lib/bsprot/switch_protocol.h
@@ -0,0 +1,19 @@
+/*
+ * switch_protocol.h
+ *
+ *  Created on: 19 jan. 2024
+ *      Author: andre
+ */
+
+#ifndef BSPROT_SWITCH_PROTOCOL_H_
+#define BSPROT_SWITCH_PROTOCOL_H_
+
+#include "protocol.h"
+
+#define BSCP_CMD_SWITCH_ONOFF		0x20
+#define BSCP_CMD_SWITCH_PWM	    	0x21
+#define BSCP_CMD_SWITCH_POLARISED	0x22
+
+
+
+#endif /* BSPROT_SWITCH_PROTOCOL_H_ */
